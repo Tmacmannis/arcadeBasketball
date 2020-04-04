93359e1a4961ca972d306a1d51bf35ae7d763578
Tmacmannis <timmacmannis@yahoo.com>
2020-04-04 18:48:16 -0400

    changes to counter board

--- NanoCounter/NanoCounter.ino
+++ NanoCounter/NanoCounter.ino
@@ -1,24 +1,38 @@
 #include <HCSR04.h>
+#include <Wire.h>
 
 UltraSonicDistanceSensor distanceSensor(12, 13);  // Initialize sensor that uses digital pins 13 and 12.
 
+bool shotMade = false;
 int count;
 
 void setup () {
     Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
+    Wire.begin(8);                // join i2c bus with address #8
+    Wire.onRequest(requestEvent); // register event
 }
 
 void loop () {
-    // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
     double distance = distanceSensor.measureDistanceCm();
     //delay(40);
     //Serial.println(distance);
     if (distance < 5){
       count++;
+      shotMade = true;
       delay(150);
-      Serial.println(count);
+      //Serial.println(count);
+      Serial.write("T");
     }
     else{
       delay(40); 
     }
+}
+
+void requestEvent() {
+  if (shotMade){
+    Wire.write("T");
+    shotMade = false;
+  } else{
+    Wire.write("F");
+  }
 }
